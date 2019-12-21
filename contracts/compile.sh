5eda7122618cc24fee38b19b3417a5261879563e
Glenn <glenn-edgar@onyxengr.com>
2019-12-21 15:16:46 -0800

    daily commit

--- contracts/compile.sh
+++ contracts/compile.sh
@@ -1,14 +1,5 @@
-#echo $1
-solcjs --optimize --bin --abi -o solc_output $1
-VAR1=$1
-VAR2="${VAR1}_HelloWorld.bin"
-VAR3="${VAR1}_HelloWorld.abi"
-VAR4="${VAR1}.bin"
-VAR5="${VAR1}.abi"
-#echo "$VAR2"
-#echo "$VAR3"
-#echo "$VAR4"
-#echo "$VAR5"
-cd solc_output
-mv $VAR2 $VAR4
-mv $VAR3 $VAR5
+# var 1 is file
+# var 2 is the contract
+
+
+solcjs --optimize --bin --abi  $1 
